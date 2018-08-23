54797e9a65ec796715fc6b86c8bae3f63940e146
Shehryarbajwa <shehryar.bajwa@gmail.com>
2018-08-23 16:36:01 -0400

    Created collectionView and Highlight Cell

--- Around Me/View/Images/CollectionView.swift
+++ Around Me/View/Images/CollectionView.swift
@@ -9,3 +9,47 @@
 import UIKit
 import Foundation
 
+extension ImageSelectorViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath) as!
+        ImageCollectionViewCell
+        cell.photo = photos[indexPath.item]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 4.5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 3, bottom: 0, right: 3)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 4.5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = (self.view.frame.width-15)/3
+        return CGSize(width: size, height: size)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        hightlightCell(indexPath, highlighted: false)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        hightlightCell(indexPath, highlighted: true)
+    }
+    
+    func hightlightCell(_ indexPath: IndexPath, highlighted: Bool){
+        guard let cell = collectionView.cellForItem(at: indexPath) as? ImageCollectionViewCell else{return}
+        cell.highlightedView.isHidden = highlighted
+        changeButtonLabel()
+    }
+}
