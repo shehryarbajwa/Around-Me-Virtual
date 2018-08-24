d6b76e10037c16ac3e8f5c5c5f3c9625f3db6528
Shehryarbajwa <shehryar.bajwa@gmail.com>
2018-08-24 16:42:37 -0400

    Perform the download of images on main thread

--- Around Me/View/Images/CollectionCellImages.swift
+++ Around Me/View/Images/CollectionCellImages.swift
@@ -18,10 +18,7 @@ class CustomImageView: UIImageView {
         
         DispatchQueue.global().async {
             guard let imageData = try? Data(contentsOf: url) else {return}
-            DispatchQueue.main.async {
-                self.image = UIImage(data: imageData)
-                completionHandler()
-            }
+            
         }
     }
 }
