b36247957a56140097a2822cdb8878bd36c4cfa5
Shehryarbajwa <shehryar.bajwa@gmail.com>
2018-08-25 16:44:44 -0400

    In MapViewController, deleting func mapview to check UI

--- Around Me/View/MapViewControllerExtensions.swift
+++ Around Me/View/MapViewControllerExtensions.swift
@@ -12,45 +12,6 @@ import MapKit
 extension MapViewController{
     
     
-    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
-        guard let annotation = view.annotation else {return}
-        
-        var currentlocation = true
-        
-        var searchlocation = true
-        
-        
-        if (currentlocation){
-            if mapView.showsUserLocation == true {
-            guard let annotation2 = (view.annotation?.coordinate.latitude.isEqual(to: (currentlocationannotation2?.latitude)!)) else {
-                    currentlocation == false
-                    return
-            }
-            guard let annotation3 = (view.annotation?.coordinate.longitude.isEqual(to: (currentlocationannotation2?.longitude)!)) else {
-                    currentlocation == false
-                    return
-            }
-            
-            if (annotation2 == true && annotation3 == true){
-            print("dog")
-            let alert = UIAlertController(title: "Please drop a pin closeby to see what people are sharing", message: "", preferredStyle: .alert)
-            let action = UIAlertAction(title: "OK", style: .destructive, handler: nil)
-            alert.addAction(action)
-            self.present(alert, animated:true)
-            } else {
-                print("User location not shared")
-                self.performSegue(withIdentifier: "imagesVC", sender: view)
-                }
-            }
-            else if (!editmode) {
-                self.performSegue(withIdentifier: "imagesVC", sender: view)
-                }
-            else{
-                mapView.deselectAnnotation(annotation, animated: true)
-                removePin(annotation)
-            }
-            }
-    }
     
     func removePin(_ annotation: MKAnnotation){
         guard let pin = getPins(annotation) else {return}
